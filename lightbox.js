66bfb9173a0d065bdb868e225b7877a31725e418
Sheldon McGrandle <developer@8cylinder.com>
2024-06-11 20:21:32 -0700

    Add usage info

--- lightbox.js
+++ lightbox.js
@@ -1,3 +1,34 @@
+/* <image-lightbox>
+ *
+ *
+ * Usage:
+ *
+ * With no attributes, the image src will be used for the lightbox
+ * image.
+ *
+ *   <image-lightbox>
+ *     <img src="somewhere/over/the/rainbow.jpg">
+ *   </image-lightbox>
+ *
+ *
+ * Optional attributes are:
+ *
+ *   src:
+ *    An alternative image to use for the lightbox image.  Mostly
+ *    so a larger image can be used for the lightbox images.
+ *
+ *   min-width (default 640):
+ *    the min width the lightbox will work.  This is so it is disabled
+ *    for mobile where this isn't necessary.
+ *
+ *   disabled:
+ *    if any value is used, the image-lightbox will be disabled.
+ *
+ *   <image-lightbox src="a/bigger/image.jpg" min-width="300" disabled="disabled">
+ *     <img src="somewhere/over/the/rainbow.jpg">
+ *   </image-lightbox>
+ *
+ */
 
 customElements.define(
   'image-lightbox',
