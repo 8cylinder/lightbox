bf4d6dfaa9e6d6cbbfec253fe80f51cbb3c22361
Sheldon McGrandle <developer@8cylinder.com>
2024-06-11 20:15:09 -0700

    Initial commit

---
+++ lightbox.js
@@ -0,0 +1,98 @@
+
+customElements.define(
+  'image-lightbox',
+  class extends HTMLElement {
+    static observedAttributes = ['src', 'min-width', 'disabled']
+    min_width = 640
+
+    styles = `
+      .lightbox-container{
+        display: grid;
+        place-content: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100dvh;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.9);
+        padding: 1rem;
+        opacity: 0;
+        transition: opacity 0.3s;
+      }
+      .showing{
+        opacity: 1;
+      }
+      .lightbox-container img{
+        max-height: 100%;
+        margin: auto;
+      }
+      .lightbox-container, .lightbox-image{
+        cursor: zoom-out;
+      }
+    `
+
+    constructor(){
+      self = super()
+
+      const DISABLED = this.getAttribute('disabled')
+      this.DISABLED = DISABLED
+      const MIN_WIDTH = this.getAttribute('min-width') || this.min_width
+      this.MIN_WIDTH = MIN_WIDTH
+      if(DISABLED || window.innerWidth < this.MIN_WIDTH){
+        return
+      }
+
+      const thumbnail_image = this.querySelector('img')
+      thumbnail_image.style.cursor = 'zoom-in'
+      this.thumbnail_image = thumbnail_image
+    }
+
+    connectedCallback(){
+      if(this.DISABLED || window.innerWidth < this.MIN_WIDTH){
+        return
+      }
+
+      let src = this.getAttribute('src')
+      if(! src){
+        src = this.thumbnail_image.src
+      }
+
+      const lightbox = document.createElement('div')
+      lightbox.setAttribute('class', 'lightbox-container')
+
+      const img = document.createElement('img')
+      img.setAttribute('src', src)
+      img.setAttribute('class', 'lightbox-image')
+
+      const style = document.createElement("style")
+      style.textContent = this.styles
+
+      lightbox.appendChild(style)
+      lightbox.appendChild(img)
+
+      let direction = 'open'
+
+      this.thumbnail_image.addEventListener('click', ()=>{
+        this.thumbnail_image.style.cursor = 'zoom-in'
+        this.appendChild(lightbox)
+
+        // if setTimeout is not used, the transition does not fire
+        setTimeout(() => {
+          lightbox.classList.toggle('showing')
+          direction = 'open'
+        }, 1);
+      })
+
+      lightbox.addEventListener('click', ()=>{
+        lightbox.classList.toggle('showing')
+        direction = 'close'
+      })
+
+      lightbox.addEventListener('transitionend', ()=>{
+        if(direction == 'close'){
+          lightbox.remove()
+        }
+      })
+    }
+  }
+)
